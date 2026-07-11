d011f4fddf6e187212123f0b0a5c7575bfde4fb4
Rishabh Kumar <renuyadavdob1991@gmail.com>
2026-07-11 12:04:06 +0530

    Personal portfolio website

---
+++ README.md
@@ -0,0 +1,2 @@
+# My-personal-portfolio-website-
+This is My personal portfolio website created using HTML , CSS , JS  (This is the experiment version  )
---
+++ script1.js
@@ -0,0 +1,44 @@
+
+function contactMe() {
+  window.location.href = "#contact";
+}
+function goHome() {
+  window.location.href = "index.html";
+}
+function goAbout() {
+  window.location.href = "about.html";
+}
+function goContact() {
+  window.location.href = "contract1.html";
+}
+function goAbout() {
+  window.location.href = "about.html";
+}
+function goProjects() {
+  window.location.href = "project.html";
+}
+function exploreProjects() {
+    window.location.href = "project.html";
+}
+
+function contactMe() {
+  window.location.href = "#contact";
+}
+function goHome() {
+  window.location.href = "index.html";
+}
+function goAbout() {
+  window.location.href = "about.html";
+}
+function goContact() {
+  window.location.href = "contract1.html";
+}
+function goAbout() {
+  window.location.href = "about.html";
+}
+function goProjects() {
+  window.location.href = "project.html";
+}
+function exploreProjects() {
+    window.location.href = "project.html";
+}
